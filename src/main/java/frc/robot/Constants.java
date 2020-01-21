fd20e650f49b416bbd108d06b77d03c70d0fefe6
Andrew Gasser <agasser01@hotmail.com>
2020-01-21 17:27:08 -0600

    Update IDs to match new bot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,10 +28,10 @@ import edu.wpi.first.wpilibj.util.Units;
 public final class Constants {
 
   public static final class DriveTrain {
-    public static final int DEVICE_ID_LEFT_MASTER = 2;
-    public static final int DEVICE_ID_LEFT_SLAVE = 0;
-    public static final int DEVICE_ID_RIGHT_MASTER = 3;
+    public static final int DEVICE_ID_RIGHT_MASTER = 0;
     public static final int DEVICE_ID_RIGHT_SLAVE = 1;
+    public static final int DEVICE_ID_LEFT_MASTER = 2;
+    public static final int DEVICE_ID_LEFT_SLAVE = 3;
 
     public static final int SENSOR_UNITS_PER_ROTATION = 4096;
     public static final double WHEEL_DIAMETER_INCHES = 6d;
