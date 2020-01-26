fecbf9f08818c8ab2c8b23939674c0b32bb49d59
Brandon Gasser <bgasser12@gmail.com>
2020-01-26 16:08:33 -0600

    Cleaned up DriveToTarget

--- src/main/java/frc/robot/commands/DriveToTargetCommand.java
+++ src/main/java/frc/robot/commands/DriveToTargetCommand.java
@@ -1,8 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrainSubsystem;
 
@@ -12,28 +9,20 @@ import frc.robot.subsystems.DriveTrainSubsystem;
 public class DriveToTargetCommand extends CommandBase {
 
   private final DriveTrainSubsystem driveTrainSubsystem;
-  private final Pose2d targetPosition;
-  private Trajectory trajectory;
   private double targetX;
   private double targetY;
-  private double targetOrientation;
   private double currentX;
   private double currentY;
 
-  public DriveToTargetCommand(DriveTrainSubsystem driveTrainSubsystem, Pose2d targetPosition) {
+  public DriveToTargetCommand(DriveTrainSubsystem driveTrainSubsystem) {
     this.driveTrainSubsystem = driveTrainSubsystem;
-    this.targetPosition = targetPosition;
     addRequirements(driveTrainSubsystem);
   }
 
   @Override
   public void initialize() {
-    // Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
-    //   driveTrainSubsystem.getCurrentPose(),
-    //   null,
-    //   targetPosition,
-    //   null);
-    System.out.println("Started Command");
+    this.targetX = driveTrainSubsystem.getSavedPose().getTranslation().getX();
+    this.targetY = driveTrainSubsystem.getSavedPose().getTranslation().getY();
   }
   
   @Override
@@ -43,38 +32,36 @@ public class DriveToTargetCommand extends CommandBase {
     double netX = currentX - targetX;
     double netY = currentY - targetY;
     double pointOrientation;
-    if (netX >= 0) {
-      if (netY >= 0) {
-        pointOrientation = -90 - Math.atan(netX/netY);
+    if (netX >= 0d) {
+      if (netY >= 0d) {
+        pointOrientation = -90d - Math.atan(netX/netY);
       } else {
-        pointOrientation = 90 + Math.atan(netX/-netY);
+        pointOrientation = 90d + Math.atan(netX/-netY);
       }
     } else {
       if (netY >= 0) {
-        pointOrientation = -90 + Math.atan(-netX/netY);
+        pointOrientation = -90d + Math.atan(-netX/netY);
       } else {
-        pointOrientation = 90 - Math.atan(-netX/-netY);
+        pointOrientation = 90d - Math.atan(-netX/-netY);
       }
      }
-     SmartDashboard.putNumber("Target Orientation: ", pointOrientation);
-     if (!(driveTrainSubsystem.getCurrentPose().getRotation().getDegrees() <= pointOrientation + 3 && driveTrainSubsystem.getCurrentPose().getRotation().getDegrees() >= pointOrientation - 3)) {
-       driveTrainSubsystem.arcadeDrive(0, .15);
+     if (driveTrainSubsystem.getCurrentPose().getRotation().getDegrees() <= pointOrientation + 3 &&
+        driveTrainSubsystem.getCurrentPose().getRotation().getDegrees() >= pointOrientation - 3) {
+      driveTrainSubsystem.arcadeDrive(.3, 0.0);
      } else {
-       driveTrainSubsystem.arcadeDrive(.3, 0);
+       driveTrainSubsystem.arcadeDrive(0.0, .15);
      }
   }
 
   @Override
   public boolean isFinished() {
     return (currentX >= targetX - .25 && currentX <= targetX + .25) &&
-      (currentY >= targetY - .25 && currentY <= targetY + .25) /*&&
-      (targetOrientation == gyroSubsystem.getGyroYaw() ||
-      targetOrientation == gyroSubsystem.getGyroYaw())*/;
+      (currentY >= targetY - .25 && currentY <= targetY + .25);
   }
 
   @Override
   public void end(boolean interrupted) {
-    driveTrainSubsystem.arcadeDrive(0, 0);
+    driveTrainSubsystem.arcadeDrive(0.0, 0.0);
   }
   
 }
