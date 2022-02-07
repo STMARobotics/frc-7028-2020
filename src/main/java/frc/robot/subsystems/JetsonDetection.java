5e15290bbec0c8818b1cc58a9e02741481d58d75
Andrew Gasser <agasser01@hotmail.com>
2022-02-07 15:25:14 -0600

    Disable object detection when robot disabled, and other Jetson cleanup

--- src/main/java/frc/robot/subsystems/JetsonDetection.java
+++ src/main/java/frc/robot/subsystems/JetsonDetection.java
@@ -5,6 +5,11 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import lombok.Getter;
 
+/**
+ * This class holds the data sent by the Jetson. The Jetson sends one big JSON string of data for a detection instead
+ * of sending each value in a separate network table entry so we are can be certain to get all matching values for a
+ * frame.
+ */
 @Getter
 @JsonNaming(UpperCamelCaseStrategy.class)
 public class JetsonDetection {
